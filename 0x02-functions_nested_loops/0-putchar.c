55608f503f82bb61eadf8cf60c99a13d1c27941a
boazww <wanjuguboaz@gmail.com>
2022-10-22 01:41:25 -0700

    Testing something out

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,15 +4,15 @@
 * Description: You are not allowed to include standard libraries.
 * Return: 0
 */
-int main(void)  
+int main(void)
 {
-char putchar[] = "_putchar"; 
+char putchar[] = "_putchar";
 int i = 0;
 while (putchar[i] != '\0')
 {
-_putchar(putchar[i]);     
+_putchar(putchar[i]);
 i++;
 }
-_putchar('\n'); 
-return (0);
+_putchar('\n');
+ return (0);
 }
