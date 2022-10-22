4a313f0121d394611566438a006564fec00bb981
boazww <wanjuguboaz@gmail.com>
2022-10-22 01:33:02 -0700

    Done with q1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,39 +1,18 @@
 #include "main.h"
-
 /**
-
- * main - Print "Holberton" followed by a new line.
-
- * Description: You are not allowed to include standard libraries.
-
- * Return: 0
-
- */
-
-int main(void)
-  
+* main - Print "Holberton" followed by a new line.
+* Description: You are not allowed to include standard libraries.
+* Return: 0
+*/
+int main(void)  
 {
-  
-char putchar[] = "_putchar";
-  
+char putchar[] = "_putchar"; 
 int i = 0;
-  
-
-  
 while (putchar[i] != '\0')
-    
 {
-      
-_putchar(putchar[i]);
-      
+_putchar(putchar[i]);     
 i++;
-      
 }
-
-_putchar('\n');
-  
-
-  
+_putchar('\n'); 
 return (0);
-  
 }
